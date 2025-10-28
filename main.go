1750b998b9949904d0333edc7ab170b69780225d
Ojo oluwatobi Daniel <doluwatobi69@gmail.com>
2025-10-28 20:00:42 +0100

    Added JSON file saving feature to program

--- main.go
+++ main.go
@@ -1,37 +1,74 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 )
 
+type User struct {
+	FirstName  string `json:"first_name"`
+	LastName   string `json:"last_name"`
+	Age        int    `json:"age"`
+	Department string `json:"department"`
+	Faculty    string `json:"faculty"`
+	Level      string `json:"level"`
+}
+
 func main() {
-	var firstName, lastName, department, faculty, level string
-	var age int
+	var user User
 
 	// Request inputs from the user
 	fmt.Print("Enter your first name: ")
-	fmt.Scanln(&firstName)
+	fmt.Scanln(&user.FirstName)
 
 	fmt.Print("Enter your last name: ")
-	fmt.Scanln(&lastName)
+	fmt.Scanln(&user.LastName)
 
 	fmt.Print("Enter your age: ")
-	fmt.Scanln(&age)
+	fmt.Scanln(&user.Age)
 
 	fmt.Print("Enter your department: ")
-	fmt.Scanln(&department)
+	fmt.Scanln(&user.Department)
 
 	fmt.Print("Enter your faculty: ")
-	fmt.Scanln(&faculty)
+	fmt.Scanln(&user.Faculty)
 
 	fmt.Print("Enter your level: ")
-	fmt.Scanln(&level)
+	fmt.Scanln(&user.Level)
 
 	// Print user details
 	fmt.Println("\n----- User Information -----")
-	fmt.Printf("Name: %s %s\n", firstName, lastName)
-	fmt.Printf("Age: %d\n", age)
-	fmt.Printf("Department: %s\n", department)
-	fmt.Printf("Faculty: %s\n", faculty)
-	fmt.Printf("Level: %s\n", level)
+	fmt.Printf("Name: %s %s\n", user.FirstName, user.LastName)
+	fmt.Printf("Age: %d\n", user.Age)
+	fmt.Printf("Department: %s\n", user.Department)
+	fmt.Printf("Faculty: %s\n", user.Faculty)
+	fmt.Printf("Level: %s\n", user.Level)
+
+	// Create data directory if it doesn't exist
+	err := os.MkdirAll("data", os.ModePerm)
+	if err != nil {
+		fmt.Println("Error creating directory:", err)
+		return
+	}
+
+	// Create a JSON file inside data/
+	file, err := os.Create("data/user_data.json")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Encode user info as JSON
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	err = encoder.Encode(user)
+	if err != nil {
+		fmt.Println("Error writing JSON:", err)
+		return
+	}
+
+	fmt.Println("\n✅ User data saved successfully to data/user_data.json")
+
 }
